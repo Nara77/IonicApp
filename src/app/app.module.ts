c22a45429ea61dd5e2c79e5cb620707a8a3ab567
Nardos <nardos.n9@gmail.com>
2018-04-25 21:37:42 -0700

    made a text to speech register and login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,32 +3,44 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { LoginPage } from '../pages/login/login'
+import { RegisterPage } from '../pages/register/register'
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { Camera } from '@ionic-native/camera';
 import { TextToSpeech } from '@ionic-native/text-to-speech';
+import { UserserviceProvider } from '../providers/userservice/userservice';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    
+
+    
+    
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    HttpClientModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    
+        
   ],
   providers: [
     StatusBar,
      Camera,
      TextToSpeech,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    UserserviceProvider
   ]
 })
 
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { TextToSpeech } from '@ionic-native/text-to-speech';
+import { RegisterPage } from '../register/register';
+import { LoginPage } from '../login/login';
+
 
 @Component({
   selector: 'page-home',
@@ -12,21 +15,33 @@ export class HomePage {
   text:string;
 
   constructor(public navCtrl: NavController, private tts: TextToSpeech) {
-    
+    //this.goToOtherPage()
   } 
    
-   sayText():Promise:any {
+   sayText() {
          this.tts.speak(this.text)
          .then(() => console.log("Success" + this.text))
           .catch((reason: any) => console.log(reason));
         
         
        }
-  
+  // ionViewDidLoad() {
+  //   console.log("Run")
+  //   this.goToOtherPage()
+
+  }
+
+
+
+      // goToOtherPage() {
+       // push another page onto the history stack
+        //causing the nav controller to animate the new page in
+        //this.navCtrl.push(LoginPage);
+      // }
       
-  
-}
+      //}
  
  
+
 
 
